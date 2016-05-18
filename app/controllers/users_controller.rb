a39b712588023353a0f9933ceb3d67fb20dddc67
jjseymour <jjseymour@gmail.com>
2016-05-18 17:57:13 -0400

    login and signup created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Signup Failed"
+      redirect_to signup_path
+    end
   end
 
   def index
   end
 
   def edit
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :username, :venmo)
   end
 end
