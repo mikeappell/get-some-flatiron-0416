ef074b4cc7737bfcdbd35f08af442fd30ed39a6b
Michael Appell <mikeappell@gmail.com>
2016-05-18 13:21:15 -0400

    Added model associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ActiveRecord::Base
+  has_many :user_orders
+  has_many :users, through: :user_orders
+  belongs_to :restaurant
+  has_many :items
+  belongs_to :organization
+  belongs_to :admin, class_name: "User"
 end
