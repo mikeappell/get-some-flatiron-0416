8e57aa0a67a1c676463c22362b192712c6f4c715
steve21168 <steve21168@gmail.com>
2016-05-21 18:50:20 -0400

    Fixed navbar when users logged out

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
 
   private
   def confirmation_token
-      if self.confirm_token.blank?
+       if self.confirm_token.blank?
           self.confirm_token = SecureRandom.urlsafe_base64.to_s
       end
   end
