a27323a41035355d4a0e71744bae2cd3a6e19913
JJ Seymour <jjseymour@gmail.com>
2016-05-22 13:23:18 -0400

    Update orders_helper.rb

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -8,11 +8,6 @@ module OrdersHelper
       else
         f.collection_select(:organization_id, @user.organizations, :id, :name, {selected: 0})
       end
-      # if @organization
-      #   f.collection_select(:user, :organization_id, @user.organizations, :id, :name, {selected: @organization.id})
-      # else
-      #   f.collection_select(:user, :organization_id, @user.organizations, :id, :name, {selected: 0})
-      # end
     end
   end
 end
