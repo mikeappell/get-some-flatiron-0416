7fca37d7cdbe62d5e25e7a76dc8bb4e439dc0ee6
snlamm <shmuel.lamm@gmail.com>
2016-05-22 18:26:46 -0400

    fix bugs in user show page

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,7 @@
 module OrdersHelper
   def order_organizations(f)
     if @user.organizations.count == 1
-      "#{f.hidden_field(:organization_id, value: @user.organizations.first.id)} #{@user.organizations.first.name}".html_safe
+      "#{f.hidden_field(:organization_id, value: @user.organizations.first.id)} #{@user.organizations.first.domain_name}".html_safe
     else
       if @organization
         f.collection_select(:organization_id, @user.organizations, :id, :name, {selected: @organization.id})
