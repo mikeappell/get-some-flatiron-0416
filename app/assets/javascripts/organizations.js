4896ae5b6db51b2289a0c550d13aa961014018e7
steve21168 <steve21168@gmail.com>
2016-05-22 16:54:00 -0400

    Working on controller action for removing association

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -10,11 +10,18 @@ function toggle() {
   $.fn.bootstrapSwitch.defaults.size = 'small'
   $.fn.bootstrapSwitch.defaults.onColor= 'success'
   $(".member").bootstrapSwitch()
-}
-
-
-function switchToggle() {
   $(".member").on('switchChange.bootstrapSwitch', function() {
-    
-  })
-}
+    if ($(this).parent().parent().hasClass('bootstrap-switch-off')) {
+        var member = false
+      } else {
+          var member = true
+      }
+
+    var groupId = $(this).val()
+
+    $.ajax({
+       url: '/groups/' + groupId,
+       type: 'PATCH',
+       data: {member: member}
+    })
+})}
