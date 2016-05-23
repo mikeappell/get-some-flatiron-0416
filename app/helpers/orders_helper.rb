7b2c690a448695b8d3d1818f02d956a0a3ff0daa
snlamm <shmuel.lamm@gmail.com>
2016-05-23 09:14:14 -0400

    Update orders_helper.rb

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,12 +1,12 @@
 module OrdersHelper
   def order_organizations(f)
     if @current_user.organizations.count == 1
-      "#{f.hidden_field(:organization_id, value: @current_user.organizations.first.id)} #{@current_user.organizations.first.name}".html_safe
+      "#{f.hidden_field(:organization_id, value: @current_user.organizations.first.id)} #{@current_user.organizations.first.domain_name}".html_safe
     else
       if @organization
-        f.collection_select(:organization_id, @current_user.organizations, :id, :name, {selected: @organization.id})
+        f.collection_select(:organization_id, @current_user.organizations, :id, :domain_name, {selected: @organization.id})
       else
-        f.collection_select(:organization_id, @current_user.organizations, :id, :name, {selected: 0})
+        f.collection_select(:organization_id, @current_user.organizations, :id, :domain_name, {selected: 0})
       end
     end
   end
