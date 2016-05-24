ce209ab20d8c17b663bf7a06891202eab6761df3
Michael Appell <mikeappell@gmail.com>
2016-05-24 12:06:24 -0400

    Order timer, plus delete item buttons

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,19 @@ class ItemsController < ApplicationController
     if @item.save
       render json: {
         item_name: @item.name, 
-        item_cost: @item.cost
+        item_cost: @item.cost_formatted,
+        item_id: @item.id
       }
     else
       error_message = @item.errors.messages
       render partial: 'shared/errors', locals: { errors: flash.now[:alert] = "Item " + error_message[:name][0] }
     end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    render json: { destroyed: true }
   end
 
   private
