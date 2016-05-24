82a906182248f3524e6d4099893c7acb71dc8c47
snlamm <shmuel.lamm@gmail.com>
2016-05-24 13:53:34 -0400

    add items channel. messages channel commented out

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,10 @@
-App.messages = App.cable.subscriptions.create('MessagesChannel', {
-  received: function(data) {
-    return $('#messages').append(this.renderMessage(data));
-  },
-  renderMessage: function(data) {
-    $('span#message-content-reset input#message_content').val('');
-    $("#messages").animate({ scrollTop: $('#messages').prop("scrollHeight")}, 1000);
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
-  }
-});
+// App.messages = App.cable.subscriptions.create('MessagesChannel', {
+//   received: function(data) {
+//     return $('#messages').append(this.renderMessage(data));
+//   },
+//   renderMessage: function(data) {
+//     $('span#message-content-reset input#message_content').val('');
+//     $("#messages").animate({ scrollTop: $('#messages').prop("scrollHeight")}, 1000);
+//     return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+//   }
+// });
