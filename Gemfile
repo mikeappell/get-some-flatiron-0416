1efd80275c237f4faf8e3107e22c519b3ad2ec06
jjseymour <jjseymour@gmail.com>
2016-05-24 09:46:09 -0400

    practicing themes

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'redis'
 gem 'rack'
 gem 'rails_12factor'
 
+gem 'font-awesome-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,27 @@
 //= require bootstrap-switch
 // require jquery.session
 // require turbolinks
+
+//= require jquery.min
+//= require moderniz.min
+//= require jquery-migrate.min
+//= require jquery.easing.1.3.min
+//= require jquery.flexslider-min
+//= require wow.min
+//= require jquery.sticky
+//= require bootstrap-hover-dropdown.min
+//= require jquery.stellar.min
+//= require owl.carousel.min
+//= require jquery.mb.YTPlayer.min
+//= require waypoints.min
+//= require easypiechart
+//= require jquery.isotope.min
+//= require jquery.imagesloaded.min
+//= require jquery.counterup.min
+//= require jquery.countdown
+//= require contact_me
+//= require jqBootstrapValidation
+//= require custom
+//= require isotope-custom
+
 //= require_tree .
