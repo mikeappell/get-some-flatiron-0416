7c43c4aeb20900345925540ef1a415d7bd0acfc9
Michael Appell <mikeappell@gmail.com>
2016-05-25 15:56:19 -0400

    Few logix fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :current_user
+  before_action :current_user, :set_default_organization
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
-    # session[:current_org] ||= @current_user.organizations.first.id
+    
     if @current_user.nil? && session[:user_id]
       session[:user_id] = nil
       redirect_to login_path
     else
       @current_user
+    end
+  end
+
+  def set_default_organization
+    if @current_user
+      session[:current_org] ||= @current_user.organizations.first.id
     end
   end
 
